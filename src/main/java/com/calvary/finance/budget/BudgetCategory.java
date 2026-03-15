8bae6c049b194bf3061f0bce4a2567f26bd5cda5
vheekey <vicformidable@gmail.com>
2026-03-15 10:34:52 +0100

    Creates BudgetCategory entity

---
+++ src/main/java/com/calvary/finance/budget/BudgetCategory.java
@@ -0,0 +1,18 @@
+package com.calvary.finance.budget;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "budget_categories")
+@Data
+@NoArgsConstructor
+public class BudgetCategory {
+    @Id
+    private String accNo;
+    private String description;
+    private boolean isActive;
+}
